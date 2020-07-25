f5f1f5d9159c1e611e66eea3b171bc3b7b354308
ConsiKot <61238187+ConsiKot@users.noreply.github.com>
2020-07-25 12:36:41 +0530

    Match Tied

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictaktoe;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,12 +17,17 @@ public class MainActivity extends AppCompatActivity
     boolean gameActive=true;
     int gameState[]={2,2,2,2,2,2,2,2,2};
     int [][]winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    Button button;
+    int count=0;
+    TextView textView;
 
 // 0 for jerry and 1 for tom and 2 for empty
+    @SuppressLint("SetTextI18n")
     public void dropIn(View view)
     {
         ImageView counter=(ImageView)view;
         int tappedCounter=Integer.parseInt(counter.getTag().toString());
+
         if(gameState[tappedCounter]==2 && gameActive)
         {
             gameState[tappedCounter]=activePlayer;
@@ -29,11 +35,13 @@ public class MainActivity extends AppCompatActivity
             counter.setTranslationY(-500);
             if(activePlayer==0)
             {
+                count+=1;
                 counter.setImageResource(R.drawable.jerry);
                 activePlayer=1;
             }
             else
             {
+                count+=1;
                 counter.setImageResource(R.drawable.tom);
                 activePlayer=0;
             }
@@ -47,17 +55,29 @@ public class MainActivity extends AppCompatActivity
                     String winner="";
                     if(activePlayer==0)
                     {
-                        winner="Tom";
+                        winner="Tom has WON !";
                     }
-                    else
+                    else if(activePlayer==1)
                     {
-                        winner="Jerry";
+                        winner="Jerry has WON !";
                     }
-                    Button button=findViewById(R.id.button);
-                    TextView textView=findViewById(R.id.textView);
-                    textView.setText(winner+" has WON !!!");
+                    button=findViewById(R.id.button);
+                    textView=findViewById(R.id.textView);
+                    textView.setText(winner);
+                    count=0;
                     button.setVisibility(View.VISIBLE);
                     textView.setVisibility(View.VISIBLE);
+                }
+                else if((gameState[winningPosition[0]]!=gameState[winningPosition[1]] || gameState[winningPosition[1]]!=gameState[winningPosition[2]] || gameState[winningPosition[0]]!=gameState[winningPosition[2]]) && gameState[winningPosition[0]]!=2 && count>9)
+                {
+                    gameActive=false;
+                    button=findViewById(R.id.button);
+                    textView=findViewById(R.id.textView);
+                    textView.setText("NO WINNER ...Both played WELL !");
+                    button.setVisibility(View.VISIBLE);
+                    count=0;
+                    textView.setVisibility(View.VISIBLE);
+
                 }
 
             }
@@ -65,8 +85,8 @@ public class MainActivity extends AppCompatActivity
     }
     public void playAgain(View view)
     {
-        Button button= findViewById(R.id.button);
-        TextView textView=findViewById(R.id.textView);
+        button= findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
         button.setVisibility(View.INVISIBLE);
         textView.setVisibility(View.INVISIBLE);
 
